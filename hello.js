125d3f86f6e7ecbfc29b1cd903fa1c67757ae201
iAmAgrim69 <113252802+iAmAgrim69@users.noreply.github.com>
2022-10-18 21:40:05 -0400

    Week 2 final

---
+++ hello.js
@@ -0,0 +1,4 @@
+const message = "Hello World!";
+exports.sayHello = function(){
+    console.log(message);
+}
---
+++ server.js
@@ -0,0 +1,12 @@
+// const hello = require('./hello');
+// hello.sayHello();
+const http = require('http');
+
+http.createServer((req,res)=>{
+res.writeHead(200,{
+    'Content-Type':'text/plain'
+});
+res.end('Hello World!');
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
